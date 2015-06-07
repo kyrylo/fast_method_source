b5528fe6d9590e43d12c5b5f8e0b7fe2322d8cb5
Kyrylo Silin <silin@kyrylo.org>
2015-06-07 19:08:05 +0300

    Add C code

---
+++ ext/fast_method_source/fast_method_source.c
@@ -0,0 +1,302 @@
+#include "fast_method_source.h"
+
+static VALUE rb_eSourceNotFoundError;
+
+static int
+read_lines(const char *filename, char **file[], const int start_line)
+{
+    FILE *fp;
+    ssize_t read;
+    char *line = NULL;
+    size_t len = 0;
+    size_t line_len = 0;
+    int line_count = 0;
+    int occupied_lines = 0;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        rb_raise(rb_eIOError, "No such file or directory - %s", filename);
+    }
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        if (line_count < start_line) {
+            line_count++;
+            continue;
+        }
+
+        if ((occupied_lines != 0) && (occupied_lines % (MAXLINES-1) == 0)) {
+            reallocate_lines(file, occupied_lines);
+        }
+
+        line_len = strlen(line);
+
+        if (line_len >= MAXLINELEN) {
+            char *tmp;
+
+            if ((tmp = realloc((*file)[occupied_lines], line_len + 1)) == NULL) {
+                rb_raise(rb_eNoMemError, "failed to allocate memory");
+            }
+
+            (*file)[occupied_lines] = tmp;
+        }
+
+        strncpy((*file)[occupied_lines], line, read);
+        (*file)[occupied_lines][read] = '\0';
+        occupied_lines++;
+    }
+
+    free(line);
+    fclose(fp);
+
+    return occupied_lines;
+}
+
+static void
+reallocate_lines(char **lines[], int occupied_lines)
+{
+    int new_size = occupied_lines + MAXLINES + 1;
+    char **temp_lines = realloc(*lines, sizeof(*temp_lines) * new_size);
+
+    if (temp_lines == NULL) {
+        rb_raise(rb_eNoMemError, "failed to allocate memory");
+    } else {
+        *lines = temp_lines;
+
+        for (int i = 0; i < MAXLINES; i++) {
+            if (((*lines)[occupied_lines + i] = malloc(sizeof(char) * MAXLINELEN)) == NULL) {
+                rb_raise(rb_eNoMemError, "failed to allocate memory");
+            }
+        }
+    }
+}
+
+static NODE *
+with_silenced_stderr(NODE *(*compile)(const char*, VALUE, int), VALUE rb_str)
+{
+    int old_stderr;
+    FILE *null_fd;
+
+    old_stderr = DUP(STDERR_FILENO);
+    fflush(stderr);
+    null_fd = fopen(null_filename, "w");
+    DUP2(fileno(null_fd), STDERR_FILENO);
+
+    NODE *node = (*compile)("-", rb_str, 1);
+
+    fflush(stderr);
+    fclose(null_fd);
+
+    DUP2(old_stderr, STDERR_FILENO);
+    close(old_stderr);
+
+    return node;
+}
+
+static NODE *
+parse_expr(VALUE rb_str) {
+    return with_silenced_stderr(rb_compile_string, rb_str);
+}
+
+static int
+filter_interp(char *line)
+{
+    int dangling = 0;
+    int brackets = 0;
+
+    for (int i = 0; line[i] != '\0'; i++) {
+        if (line[i] == '#') {
+            if (line[i + 1] == '{') {
+                brackets++;
+                dangling = 1;
+
+                line[i] = SAFE_CHAR;
+
+                if (line[i - 1] == '\\')
+                    line[i - 1] = SAFE_CHAR;
+            }
+        }
+
+        if (dangling == 1 && line[i] == '}') {
+            brackets--;
+
+            if (brackets == 0) {
+                dangling = 0;
+                line[i] = SAFE_CHAR;
+            }
+        } else if (brackets > 0) {
+            line[i] = SAFE_CHAR;
+        }
+    }
+
+    return brackets;
+}
+
+static int
+contains_end_kw(const char *line)
+{
+    char *match;
+    char prev_ch;
+
+    if ((match = strstr(line, "end")) != NULL) {
+        prev_ch = (match - 1)[0];
+        return prev_ch == ' ' || prev_ch == '\0' || prev_ch == ';';
+    } else {
+        return 0;
+    }
+}
+
+static int
+is_accessor(const char *line)
+{
+    return strstr(line, "attr_reader") != NULL ||
+        strstr(line, "attr_writer") != NULL ||
+        strstr(line, "attr_accessor") != NULL;
+}
+
+static int
+is_comment(const char *line)
+{
+    size_t line_len = strlen(line);
+
+    for (size_t i = 0; i < line_len; i++) {
+        if (line[i] == ' ')
+            continue;
+
+        if (line[i] == '#') {
+            for (size_t j = i - 1; j != 0; j--) {
+                if (line[j] != ' ')
+                    return 0;
+            }
+
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+static int
+is_static_definition(const char *line)
+{
+    return strstr(line, " def ") != NULL || strncmp(line, "def ", 4);
+}
+
+static VALUE
+find_expression(char **file[], const int occupied_lines)
+{
+    unsigned expr_size = occupied_lines * MAXLINELEN;
+    char expr[expr_size];
+    char parseable_expr[expr_size];
+    char *first_line = (*file)[0];
+    char *line = NULL;
+    int should_parse;
+    int dangling_brackets = 0;
+
+    expr[0] = '\0';
+    parseable_expr[0] = '\0';
+
+    if (is_static_definition(first_line)) {
+        should_parse = 1;
+    } else if (is_accessor(first_line)) {
+        should_parse = 1;
+    } else {
+        should_parse = 0;
+    }
+
+    for (int i = 0; i < occupied_lines; i++) {
+        line = (*file)[i];
+
+        strcat(expr, line);
+
+        if (is_comment(line))
+            continue;
+
+        if (dangling_brackets)
+            continue;
+
+        dangling_brackets = filter_interp(line);
+        strcat(parseable_expr, line);
+
+        if (dangling_brackets)
+            continue;
+
+        if (should_parse || contains_end_kw(line)) {
+            if (parse_expr(rb_str_new2(parseable_expr)) != NULL) {
+                return rb_str_new2(expr);
+            }
+        }
+    }
+
+    free_memory_for_file(file, occupied_lines);
+    rb_raise(rb_eSyntaxError, "failed to parse expression (probably a bug)");
+
+    return Qnil;
+}
+
+static char **
+allocate_memory_for_file(void)
+{
+    char **file;
+
+    if ((file = malloc(sizeof(*file) * MAXLINES)) == NULL) {
+        rb_raise(rb_eNoMemError, "failed to allocate memory");
+    }
+
+    for (int i = 0; i < MAXLINES; i++) {
+        if ((file[i] = malloc(sizeof(char) * MAXLINELEN)) == NULL) {
+            rb_raise(rb_eNoMemError, "failed to allocate memory");
+        };
+    }
+
+    return file;
+}
+
+static void
+free_memory_for_file(char **file[], const int occupied_lines)
+{
+    int lines_to_free = occupied_lines >= MAXLINES ? occupied_lines : MAXLINES;
+
+    for (int i = 0; i < lines_to_free; i++) {
+        free((*file)[i]);
+    }
+
+    free(*file);
+}
+
+static VALUE
+mMethodExtensions_source(VALUE self)
+{
+    VALUE method = rb_iv_get(self, "@method");
+    VALUE source_location = rb_funcall(method, rb_intern("source_location"), 0);
+    VALUE name = rb_funcall(method, rb_intern("name"), 0);
+
+    if (NIL_P(source_location)) {
+        rb_raise(rb_eSourceNotFoundError, "Could not locate source for %s!",
+                 RSTRING_PTR(rb_sym2str(name)));
+    }
+
+    char **file = allocate_memory_for_file();
+
+    const char *filename = RSTRING_PTR(RARRAY_AREF(source_location, 0));
+    const int start_line = FIX2INT(RARRAY_AREF(source_location, 1)) - 1;
+
+    const int occupied_lines = read_lines(filename, &file, start_line);
+
+    VALUE expression = find_expression(&file, occupied_lines);
+
+    free_memory_for_file(&file, occupied_lines);
+
+    return expression;
+}
+
+void Init_fast_method_source(void)
+{
+    VALUE rb_mFastMethodSource = rb_define_module_under("FastMethodSource", rb_cObject);
+
+    rb_eSourceNotFoundError = rb_define_class_under(rb_mFastMethodSource,"SourceNotFoundError", rb_eStandardError);
+    VALUE rb_mMethodExtensions = rb_define_module_under(rb_mFastMethodSource, "MethodExtensions");
+
+    rb_define_method(rb_mMethodExtensions, "source", mMethodExtensions_source, 0);
+}
---
+++ ext/fast_method_source/fast_method_source.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ruby.h>
+#include "node.h"
+
+#ifdef _WIN32
+#include <io.h>
+static const char *null_filename = "NUL";
+#define DUP(fd) _dup(fd)
+#define DUP2(fd, newfd) _dup2(fd, newfd)
+#else
+#include <unistd.h>
+static const char *null_filename = "/dev/null";
+#define DUP(fd) dup(fd)
+#define DUP2(fd, newfd) dup2(fd, newfd)
+#endif
+
+#define MAXLINES 1000
+#define MAXLINELEN 300
+
+static int read_lines(const char *filename, char **file[], const int start_line);
+static void reallocate_lines(char **lines[], int occupied_lines);
+static VALUE find_expression(char **lines[], const int end_line);
+static VALUE mMethodExtensions_source(VALUE self);
+static NODE *parse_expr(VALUE rb_str);
+static NODE *with_silenced_stderr(NODE *(*compile)(const char*, VALUE, int),
+				  VALUE rb_str);
+static int filter_interp(char *line);
+static char **allocate_memory_for_file(void);
+static void free_memory_for_file(char **file[], const int occupied_lines);
+static int contains_end_kw(const char *line);
+static int is_comment(const char *line);
+static int is_static_definition(const char *line);
+static int is_accessor(const char *line);
+
+#define SAFE_CHAR 'x'
