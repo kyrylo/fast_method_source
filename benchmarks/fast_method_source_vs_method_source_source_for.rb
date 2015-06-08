acb4ce9cd84a005cda927740665a364ee4334f86
Kyrylo Silin <silin@kyrylo.org>
2015-06-09 02:06:10 +0300

    benchmark: separate the bm code from the bootstrapping code

--- benchmarks/fast_method_source_vs_method_source_source_for.rb
+++ benchmarks/fast_method_source_vs_method_source_source_for.rb
@@ -1,101 +1,13 @@
-# coding: utf-8
-require 'benchmark'
-
-def require_dep(dep_name)
-  require dep_name
-rescue LoadError => e
-  name = e.message.scan(/(?<=-- ).+/).first
-  raise e unless name
-  name.gsub!('/', '-')
-  puts "'#{name}' is not installed. To fix: gem install #{name}"
-  exit
-end
-
-module Kernel
-  def suppress_warnings
-    original_verbosity = $VERBOSE
-    $VERBOSE = nil
-    result = yield
-    $VERBOSE = original_verbosity
-    return result
-  end
-end
-
-require_dep 'require_all'
-require_dep 'method_source'
-require_dep 'system_navigation'
-require_dep 'sys/cpu'
-
-stdlib_files = File.join(ENV['RUBY_ROOT'], "lib/ruby/2*/**/*.rb")
-IGNORE_LIST = [
-  /tkmacpkg.rb/,
-  /macpkg.rb/,
-  /debug.rb/,
-  /multi-tk.rb/,
-  /tk.rb/,
-  /tkwinpkg.rb/,
-  /winpkg.rb/,
-  /ICONS.rb/i,
-  /tkextlib/,
-  /profile.rb/,
-  /formatter_test_case.rb/,
-  /test_case.rb/,
-  /xmlparser/,
-  /xmlscanner/,
-  /cgi_runner.rb/,
-  /multi-irb.rb/,
-  /subirb.rb/,
-  /ws-for-case-2.rb/,
-  /test_utilities.rb/,
-  /irb\/frame.rb/,
-  /psych\/parser.rb/,
-  /rake\/contrib\/sys.rb/,
-  /rake\/gempackagetask.rb/,
-  /rake\/rdoctask.rb/,
-  /ruby182_test_unit_fix.rb/,
-  /rake\/runtest.rb/
-]
-
-files_to_require = Dir.glob(stdlib_files).reject do |p|
-  IGNORE_LIST.any? { |pattern| pattern =~ p }
-end
-
-if $DEBUG
-  print "Ignoring these files: "
-  p Dir.glob(stdlib_files) - files_to_require
-
-  puts "Requiring the rest of stdlib..."
-
-  require_all files_to_require
-
-  puts "Requiring fast_method_source from source..."
-  require_relative '../lib/fast_method_source'
-else
-  suppress_warnings { require_all files_to_require }
-end
-
-puts "Your processor is: #{Sys::CPU.processors.first.model_name}"
-puts "Counting the number of sample methods..."
-
-method_list = SystemNavigation.new.all_methods.select do |method|
-  if method.source_location
-    begin
-      FastMethodSource.source_for(method)
-    rescue
-    end
-  end
-end
-
-puts "Sample methods: #{method_list.count}"
+require_relative 'bench_helper'
 
 Benchmark.bmbm do |bm|
-  bm.report('fast_method_source') do
+  bm.report('FastMethodSource#source') do
     method_list.each do |method|
       FastMethodSource.source_for(method)
     end
   end
 
-  bm.report('method_source') do
+  bm.report('MethodSource#source') do
     method_list.each do |method|
       method.source
     end
