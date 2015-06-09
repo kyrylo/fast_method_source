62370683a1b21276883befbee5ff8fb2893f272c
Kyrylo Silin <silin@kyrylo.org>
2015-06-10 00:52:58 +0300

    ext: tighten the constants a bit

--- ext/fast_method_source/fast_method_source.h
+++ ext/fast_method_source/fast_method_source.h
@@ -16,9 +16,9 @@ static const char *null_filename = "/dev/null";
 #define DUP2(fd, newfd) dup2(fd, newfd)
 #endif
 
-#define MAXLINES 1200
-#define MAXLINELEN 120
-#define COMMENT_SIZE 1200
+#define MAXLINES 600
+#define MAXLINELEN 90
+#define COMMENT_SIZE 500
 
 typedef struct {
     int forward  : 1;
